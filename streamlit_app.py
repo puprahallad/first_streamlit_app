7d375158fcdb9e7949a5ab2aee66046a87dbbfdc
puprahallad <109089295+puprahallad@users.noreply.github.com>
2022-07-20 20:02:17 +0530

    print json response to pandas dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,4 +25,9 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+#streamlit.text(fruityvice_response.json())
+
+# Json response to pandas dataframe
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# print as table
+streamlit.dataframe(fruityvice_normalized)
