ee4a6f1b233f19fdfaae63f7bf55a51e1909d03d
puprahallad <109089295+puprahallad@users.noreply.github.com>
2022-07-27 22:36:15 +0530

    stop sf code

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title('My Parents New Healthy Dinner')
 
@@ -35,7 +36,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # print as table
 streamlit.dataframe(fruityvice_normalized)
 
-
+streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
